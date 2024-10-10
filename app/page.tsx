dceb99a28b85fc369ccb37222bff04b2298ac47b
Hyden Polikoff <hyden.dev@gmail.com>
2024-10-10 15:48:49 -0700

    finished table

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
+"use client";
+
+import { Table } from "@/components/table/Table";
 import styles from "./page.module.css";
+import { DATA } from "@/data";
+import { ColumnDef } from "@/components/table/type";
+import { useState } from "react";
+
+const COLUMNS: ColumnDef[] = [
+  {
+    key: "url",
+    title: "URL",
+    width: 100,
+    maxWidth: 500,
+    rightIcon: "open_in_new",
+    action: (row) => {
+      window.open(
+        `https://${row.url}` as string,
+        "_blank",
+        "noopener,noreferrer"
+      );
+    },
+  },
+  {
+    key: "totalCount",
+    title: "Total Count",
+    width: 1,
+  },
+  { key: "totalVisitorCount", title: "Total Visitor Count", width: 1 },
+  { key: "bounceCount", title: "Bounce Count", width: 1 },
+  { key: "startsWithCount", title: "Starts With Count", width: 1 },
+  { key: "endsWithCount", title: "Ends With Count", width: 1 },
+  { key: "avgScrollPercentage", title: "Average Scroll Percentage", width: 1 },
+  { key: "totalPageviewCount", title: "Total Pageview Count", width: 1 },
+  { key: "score", title: "Score", width: 1 },
+];
 
 export default function Home() {
+  const data = DATA.map((row) => ({
+    ...row,
+    score: (row.totalVisitorCount - row.bounceCount).toFixed(2),
+  }));
+
+  const [withPagination, setWithPagination] = useState(false);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+        <h1>Dashboard</h1>
+        <button
+          style={{
+            cursor: "pointer",
+            width: 150,
+            padding: "4px 8px",
+          }}
+          onClick={() => setWithPagination(!withPagination)}
+        >
+          {withPagination ? "Without pagination" : "With pagination"}
+        </button>
+        <Table
+          rows={data}
+          columns={COLUMNS}
+          pagination={withPagination ? { pageSize: 10 } : undefined}
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
